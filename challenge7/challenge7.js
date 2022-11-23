8402a1dce7f39589640abd747c2f3cf0810bf6b8
Hector <67712103+Hect0r1@users.noreply.github.com>
2022-11-23 07:45:57 -0600

    Update challenge7.js

--- challenge7/challenge7.js
+++ challenge7/challenge7.js
@@ -1,7 +1,7 @@
 clickNum = 0;
 
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(405, 400)
 }
 
 function draw() {
@@ -15,31 +15,33 @@ function draw() {
    bezier(390,63,287,64,224,72,204,342);
   }
   if(clickNum == 2){
-    yDeformation = 10;
-    xDeformation = 10;
+    yDeformation = 20;
+    xDeformation = 20;
     fill(220);
     steps = 10;
     for (i = 0; i <= steps; i++) {
       if(i == steps){
-        yDeformation = 10;
+        yDeformation = 20;
         xDeformation = 40
       }
       t = i / float(steps);
       x = bezierPoint(18, 121, 184, 204, t);
       y = bezierPoint(63, 64, 72, 342, t); 
+      fill(0,0,255);
       ellipse(x,y,xDeformation, yDeformation);
       yDeformation+= 3;
     }
-    yDeformation = 10;
+    yDeformation = 20;
     xDeformation = 40;
     for (i = 0; i <= steps; i++) {
       if(i == 1){
-        xDeformation = 10;
-        yDeformation = 37
+        xDeformation = 20;
+        yDeformation = 47
       }
       t = i / float(steps);
       x = bezierPoint(204, 224, 287, 390, t);
       y = bezierPoint(342, 72, 64 ,63, t);
+      fill(0,0,255);
       ellipse(x, y, xDeformation, yDeformation);
       yDeformation -= 3;
     }
@@ -57,16 +59,14 @@ async function animation(x1, y1, x2, y2, x3, y3, x4, y4){
   for(i = 0; i <= steps; i++){
     await sleep(60);
     if(i == steps){
-        yDeformation = 10;
+        yDeformation = 20;
         xDeformation = 40
     }
     t = i / float(steps);
     background(220);
-    noFill();
-    bezier(204,342, 184,72, 121, 64, 18,63);
-    bezier(390,63,287,64,224,72,204,342);
     x = bezierPoint(x1, x2, x3, x4, t);
     y = bezierPoint(y1, y2, y3, y4, t);
+    fill(0,0,255);
     ellipse(x, y, xDeformation, yDeformation);
     yDeformation+= 0.5
   }
@@ -82,19 +82,17 @@ async function animationPt2(x1, y1, x2, y2, x3, y3, x4, y4){
   for(i = 0; i <= steps; i++){
     await sleep(60);
     if(i == 1){
-      xDeformation = 10;
-      yDeformation = 37;
+      xDeformation = 20;
+      yDeformation = 47;
     }
     else if(i == steps){
-      yDeformation = 10
+      yDeformation = 20
     }
     t = i / float(steps);
     background(220);
-    noFill();
-    bezier(204,342, 184,72, 121, 64, 18,63);
-    bezier(390,63,287,64,224,72,204,342);
     x = bezierPoint(x1, x2, x3, x4, t);
     y = bezierPoint(y1, y2, y3, y4, t);
+    fill(0,0,255);
     ellipse(x, y, xDeformation, yDeformation);
     yDeformation -= 0.5
   }
